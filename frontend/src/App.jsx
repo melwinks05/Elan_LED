57c85968f77c1bbd36ab6b485a7c5b1b08fd820e
Melwin K S <melwinks75@gmail.com>
2026-08-30 12:52:51 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App(){
+  const [message, setMessage] = useState('')
+
+  useEffect(()=>{
+    fetch('http://127.0.0.1:8000/api')
+    .then(response => response.json())
+    .then(data => setMessage(data.message))
+    .catch(error => console.error('Error fetching message:', error))
+
+  }, [])
+
+  return (
+  <div>
+    <h1>Message from backend:</h1>
+    <p>{message || 'Loading...'}</p>
+  </div>
+)
+
+}
+
+
+
+export default App
